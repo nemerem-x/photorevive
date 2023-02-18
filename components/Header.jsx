5002cf4e8d288616c97f4241146a262b2df6519c
nemerem-x <pilotprecious@gmail.com>
2023-02-18 22:39:44 +0100

    logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,13 @@
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
+import Image from 'next/image'
+import logo from '/public/logo.png'
 
 export default function Header() {
 
   return (
     <div className={styles.header}>
-        <Link href='/'><h1>PhotoRevive</h1></Link>
+        <Link href='/'><Image src={logo} width='200'/></Link>
         <p>GFP-GAN</p>
     </div>
   )
